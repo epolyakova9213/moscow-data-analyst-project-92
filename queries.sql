6adf2409d75db0ed4230da3ffcf5835ff688e15b
epolyakova9213 <epolyakova9213@gmail.com>
2025-11-03 13:25:23 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(customer_id) AS customers_count
+FROM customers;
+/*запрос выбирает и подсчитывает количество покупателей из таблицы покупателей,
+отображая результат в новом столбце customers_count*/
