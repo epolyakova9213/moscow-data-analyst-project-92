6ca59f524edc9de5903a5e8f8fd8df8799264a1d
epolyakova9213 <epolyakova9213@gmail.com>
2025-11-17 00:32:18 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -48,3 +48,57 @@ ORDER BY
     END;
 /*запрос выводит ФИО продавца и его выручку за день 
  по дням недели с сортировкой по хронологии недели*/
+
+SELECT 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+        ELSE 'Другие'
+    END as age_category,
+    COUNT(*) as category_count
+FROM customers
+GROUP BY 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+        ELSE 'Другие'
+    END
+ORDER BY age_category;
+--отчет сегментирует клиентов на категории по возрасту с подсчетом кол-ва клиентов
+
+select
+to_char(sale_date, 'YYYY-MM') as selling_month,
+count(distinct customer_id) as total_customers,
+sum (quantity) as income
+from sales s 
+group by to_char(sale_date, 'YYYY-MM')
+order by to_char(sale_date, 'YYYY-MM') asc;
+--отчет показывает кол-во уникальных покупателей и выручку в месяц года
+
+with first_purchases as (
+select 
+s.customer_id,
+min(sale_date) as first_sale_date,
+p.product_id,
+p.price
+from products p
+left join sales s on p.product_id=s.product_id
+where p.price=0
+and customer_id is not null
+group by customer_id, p.product_id
+)
+select distinct on (c.customer_id)
+concat(c.first_name,' ',c.last_name) as customer,
+fp.first_sale_date as sale_date,
+concat(e.first_name,' ',e.last_name) as seller
+from first_purchases fp
+join sales s on fp.customer_id=s.customer_id 
+            and fp.first_sale_date=s.sale_date
+join customers c on fp.customer_id=c.customer_id
+join employees e on s.sales_person_id=e.employee_id 
+order by c.customer_id;
+/*выбирает в CTE покупателей с первой покупкой товара с ценой 0
+потом в основном запросе уже джойнит из таблиц нужные поля, но с условием уникальности по customer_id,
+потому что покупатели в свой первый день делали по нескольк покупок товаров с ценой 0*/
