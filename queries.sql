4dd617a70a55f8fae666eb28575e4af96a1105c2
epolyakova9213 <epolyakova9213@gmail.com>
2025-11-28 15:43:42 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -4,13 +4,13 @@ FROM customers;
 отображая результат в новом столбце customers_count*/
 
 SELECT 
-    CONCAT(e.first_name, ' ', e.middle_initial, ' ', e.last_name) AS seller,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
     COUNT(s.sales_id) as operations,
     SUM(s.quantity) as income
 FROM employees e 
 INNER JOIN sales s ON e.employee_id = s.sales_person_id  
 GROUP BY e.first_name, e.middle_initial, e.last_name    
-ORDER BY income DESC
+ORDER BY income ASC
 LIMIT 10;
 --отчет о десятке лучших продавцов
 
